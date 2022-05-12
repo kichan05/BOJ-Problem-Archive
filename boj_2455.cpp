ae3189ffe70a668f648a9c49117e6696010051c9
Heechan <ckstmznf@naver.com>
2022-05-12 23:28:43 +0900

    2022-0512
    2455번

---
+++ boj_2455.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int in, out, sum = 0, max = 0;
+    
+    for(int i = 0; i < 4; i++){
+        scanf("%d %d", &out, &in);
+        
+        sum -= out;
+        sum += in;
+
+        // printf("sum : %d\n", sum);
+      
+        if(max < sum){
+            max = sum;
+        }
+    }
+    
+    printf("%d", max);
+}
