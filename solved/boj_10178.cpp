e2f90d43a2a7ae7463734dbb244201435e404375
Heechan <ckstmznf@naver.com>
2022-07-12 08:46:33 +0900

    2022-0712
    10178번

---
+++ solved/boj_10178.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T, c, v;
+    
+    scanf("%d", &T);
+    while(T--){
+        scanf("%d %d", &c, &v);
+        
+        printf("You get %d piece(s) and your dad gets %d piece(s).\n", c / v, c % v);
+    }
+
+    return 0;
+}
