32b0b67c6c4b296d2ae7648a300d1ea311bd52c2
바키찬 <ckstmznf@naver.com>
2024-02-11 22:46:16 +0900

    2024-0211

---
+++ solved/boj_27889.py
@@ -0,0 +1,10 @@
+a = input()
+
+if a == "NLCS":
+    print("North London Collegiate School")
+if a == "BHA":
+    print("Branksome Hall Asia")
+if a == "KIS":
+    print("Korea International School")
+if a == "SJA":
+    print("St. Johnsbury Academy")
