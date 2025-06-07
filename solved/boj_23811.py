e5e3ba6f52bb78e33c01ea5b91fe3e97a01601aa
바키찬 <ckstmznf@naver.com>
2025-06-07 13:59:20 +0900

    Feat: 2025.06.07-1

---
+++ solved/boj_23811.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+for _ in range(N):
+    print("@" * N * 5)
+    
+for _ in range(N):
+    print("@" * N)
+    
+for _ in range(N):
+    print("@" * N * 5)
+    
+for _ in range(N):
+    print("@" * N)
+    
+for _ in range(N):
+    print("@" * N * 5)
